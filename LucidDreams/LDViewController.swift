7b4e8a3bb0ed762657394e367e210686b2f0d861
Pablo Episcopo <pabloepi14@gmail.com>
2016-07-10 14:37:33 -0300

    First commit.

---
+++ LucidDreams/LDViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LDViewController.swift
+//  LucidDreams
+//
+//  Created by Pablo on 7/10/16.
+//
+//
+
+import UIKit
+
+class LDViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
---
+++ Podfile
@@ -0,0 +1,9 @@
+# Uncomment this line to define a global platform for your project
+platform :ios, '9.0'
+
+target 'LucidDreams' do
+  use_frameworks!
+
+  pod 'ReactiveCocoa', '~> 4.2'
+  pod 'Alamofire', '~> 3.4'
+end
